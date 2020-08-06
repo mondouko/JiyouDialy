25e5eb6e31dd7f4e8a412058e9aaa593ef815d3a
Mondouko_team <50603371+mondouko@users.noreply.github.com>
2020-08-06 16:21:53 +0900

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -43,7 +43,7 @@ $("#submit").on("click",function(){
 	$(thislog).append('<div class = "l-ing"><p>食品名と摂取量</p></div>');
 	for(i=0;i < $("body .edit").eq(0).children().children(".ingredients").length;i++){
 		if($(input).eq(i * 2 + 3).prop("value") != "" && $(input).eq(i * 2 + 4).prop("value") > 0){
-			$(thislog).children(".l-ing").append('<div><p>' + $(input).eq(i * 2 + 3).prop("value") + '</p><p>' + Math.floor(Number($(input).eq(i * 2 + 4).prop("value")) * portion) + 'g</p></div>');
+			$(thislog).children(".l-ing").append('<div data-fg = "' + $(input).eq(i + 3).prop("value") + '"><p>' + $(input).eq(i * 2 + 3).prop("value") + '</p><p>' + Math.floor(Number($(input).eq(i * 2 + 4).prop("value")) * portion) + 'g</p></div>');
 		}
 		
 	}
@@ -52,4 +52,73 @@ $("#submit").on("click",function(){
 
 $(document).on("click",".l-ing > p",function(){
 	$(this).parent().children("div").slideToggle(300);
+	$(this).toggleClass("show");
 });
+
+
+var cdata1 = [];
+var cdata2 = [];
+var sum = 0;
+function makechart(logind){
+	cdata1 = [];
+	cdata2 = [];
+	
+	var thislog = $("#log form").eq(logind);
+	if($("#gender").val() == "m"){
+		cdata1 = fsm[Number($("#age").val())].split(",");
+	}else if($("#gender").val() == "f"){
+		cdata1 = fsf[Number($("#age").val())].split(",");
+	}
+
+	$.each(cdata1,function(ind,val){
+		sum = 0;
+		$.each($(thislog).children(".l-ing").children("div"),function(ind2,val2){
+			if($(thislog).children(".l-ing").children("div").eq(ind2).data("fg") == ind){
+				sum = sum + Number($(thislog).children(".l-ing").children("div").eq(ind2).children("p").eq(1).text());
+			}
+		});
+		//if(sum > cdata1[ind] / 3){
+		//	cdata2.push(100);
+		//}else{
+		console.log(sum)
+		cdata2.push(sum * 300 / cdata1[ind]);
+		//}
+		
+		
+	});
+
+	var ctx = $(".b-sheet");
+	/*switch($(thislog).class){
+		case "breakfast":
+			var bgc = "#3c8aaf2";
+			break;
+
+		case "lunch":
+			var bgc = "#d3890f";
+			break;
+
+		case "dinner":
+			var bgc = "#75584e";
+			break;
+
+		case "snak":
+			var bgc = "#869b32";
+			break;
+	}*/
+	new Chart(ctx, {
+		type:"radar",
+		data:{
+			labels:["乳・乳製品","卵","魚介・肉","豆・豆製品","野菜","いも類","果物","穀物","油脂","砂糖"],
+			datasets:[{
+				label:"めやすと摂取量の割合",
+				data:cdata2,
+				backgroundColor:"rgba(60,138,175,0.5)",
+				borderWidth:2,
+				pointBackgroundColor:"rgba(60,138,175,1)",
+				borderColor:"rgba(60,138,175,1)",
+				pointWidth:1
+			}]
+		}
+	});
+}
+makechart(0);
