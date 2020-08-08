c33af02f7172d88881bda54a3a680db37f7508be
Mondouko_team <50603371+mondouko@users.noreply.github.com>
2020-08-08 22:16:44 +0900

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -15,7 +15,7 @@ var code = "";
 localStorage.setItem("#log","");
 
 function savetows(code){
-	localStorage.setItem("#log",code);
+	localStorage.setItem("log",code);
 	localStorage.setItem("age",Number($("#age").val()));
 	localStorage.setItem("gender",Number($("#gender").val()));
 }
@@ -57,6 +57,13 @@ function loading(){
 		for(var i=0;i < block2.length;i=i+3){
 			$(thislog).children(".l-ing").append('<div data-fg = "' + block2[i] + '"><p>' + block2[i + 1] + '</p><p>' + block2[i + 2] + '</p></div>')
 		}
+		var ltime = $("#log form").eq(0).children(".l-time");
+		var logdate = new Date($("#log form").eq(0).data("year"),Number($(ltime).children(".l-month").text()) - 1,$(ltime).children(".l-date").text(),$.inArray($(ltime).children(".l-bld").text(),bld),0,0);
+		var ltime = $("#log form").eq(1).children(".l-time");
+		var logdate2 = new Date($("#log form").eq(1).data("year"),Number($(ltime).children(".l-month").text()) - 1,$(ltime).children(".l-date").text(),$.inArray($(ltime).children(".l-bld").text(),bld),0,0);
+		if(logdate = logdate2){
+			$("#log form").eq(1).addClass("sametime")
+		}
     });
 }
 if(localStorage.getItem("#log") != ""){
@@ -95,6 +102,9 @@ $("#submit").on("click",function(){
 			var ltime = $("#log form").eq(ind).children(".l-time");
 			var logdate2 = new Date($("#log form").eq(ind).data("year"),Number($(ltime).children(".l-month").text()) - 1,$(ltime).children(".l-date").text(),$.inArray($(ltime).children(".l-bld").text(),bld),0,0);
 			if(logdate - logdate2 >= 0){
+				if(logdate - logdate2 == 0){
+					$("#log form").eq(ind).addClass("sametime");
+				}
 				return false;
 			}
 			lognum = lognum + 1;
@@ -102,7 +112,10 @@ $("#submit").on("click",function(){
 		
 	}
 	var container = '<form class = "edit ' +  bldclass[Number($(select).eq(2).prop("value"))] + '" onsubmit="return false" data-sum="' + $(input).eq(1).prop("value") + '" data-portion="' + $(input).eq(2).prop("value") + '" data-year = "' + $("#editform .year").children("input").prop("value")+ '"><p class = "l-time"><span class = "l-month">' + $(select).eq(0).prop("value") + '</span>月<span class = "l-date">' + $(select).eq(1).prop("value") + '</span>日　<span class = "l-bld">' + bld[Number($(select).eq(2).prop("value"))] + '</span></p><h2 class = "l-ingname">' + $(input).eq(0).prop("value") + '</h2></div>';
-	if(lognum == $("#log form").length){
+	if($("#log form").length == 0){
+		alert();
+		$("#log h1").after(container);
+	}else if(lognum == $("#log form").length){
 		$("#log form").eq(lognum).after(container);
 	}else{
 		$("#log form").eq(lognum).before(container);
