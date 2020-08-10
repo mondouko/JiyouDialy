4bf82d4fe8f58f99f77166cdd4f61bb2bd3c4ae8
Mondouko_team <50603371+mondouko@users.noreply.github.com>
2020-08-10 10:09:50 +0900

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -40,9 +40,18 @@ function loading(){
     			break;
     	}
 
-    	$("#log").append('<form class = "edit ' + bldclass[bldind] + '" onsubmit = "return false" data-sum = "' + block1[4] + '" data-portion = "' + block1[5] + '" data-year = "' + block3[1] + '"></form>');
+    	$("#log").append('<form class = "edit ' + bldclass[bldind] + '" onsubmit = "return false" data-sum = "' + block1[4] + '" data-portion = "' + block1[5] + '" data-year = "' + block3[1] + '" data-date = "' + block3[1] + "," + block1[0] + "," + block1[1] + '"></form>');
     	var thislog = $("#log form").last();
+    	if(ind == 0 || $("#log form").eq($("#log form").length - 2).data("date") != $(thislog).data("date")){
+    		if(window.innerWidth > 600){
+    			$(thislog).before('<div class = "daylog" data-date = "' + $(thislog).data("date") + '"><p>' + block1[0] + "月" + block1[1] + "日の合計" + '</p><canvas width="400" height="200"></canvas></div>');
+			}else{
+				$(thislog).before('<div class = "daylog" data-date = "' + $(thislog).data("date") + '"><p>' + block1[0] + "月" + block1[1] + "日の合計" + '</p><canvas width="200" height="200"></canvas></div>');
+			}
+    	}
+    		
     	
+   
     	$(thislog).append('<p class = "l-time"><span class = "l-month">' + block1[0] + '</span>月<span class = "l-date">' + block1[1] + '</span>日　<span class = "l-bld">' + bld[bldind] + '</span></p>');
     	$(thislog).append('<h2 class = "l-ingname">' + block1[3] + '</h2>');
     	$(thislog).append('<div class = "l-chart"><p>食品群別摂取量チャート</p></div>');
@@ -69,8 +78,12 @@ function loading(){
 if(localStorage.getItem("log") != ""){
 	loading();
 	$.each($("#log form"),function(ind,val){
-		makechart(ind);
+		makechart(ind,0);
 	});
+
+	for(var i=0;i < $("#log .daylog").length;i++){
+		makechart(i,1);
+	}
 }
 function makecode(){
 	code = "";
@@ -149,7 +162,7 @@ $("#submit").on("click",function(){
 			$(thislog).children(".l-ing").append('<div data-fg = "' + $(select).eq(i + 3).val() + '"><p>' + $(input).eq(i * 2 + 3).prop("value") + '</p><p>' + Math.floor(Number($(input).eq(i * 2 + 4).prop("value")) * portion) + '</p></div>');
 		}
 	}
-	makechart(lognum);
+	makechart(lognum,0);
 	$(thislog).append('<p class = "l-comment">' + $(".comment").eq(0).val() + '</p>');
 	makecode();
 	savetows(code);
@@ -162,12 +175,19 @@ $(document).on("click",".l-ing > p",function(){
 	$(this).parent().children("div").slideToggle(300);
 	$(this).toggleClass("show");
 });
+$(document).on("click",".daylog > p",function(){
+	$(this).parent().children("canvas").slideToggle(300);
+	$(this).toggleClass("show");
+});
 
 
 var cdata1 = [];
 var cdata2 = [];
 var sum = 0;
-function makechart(logind){
+
+
+
+function makechart(logind,op){
 	if($("#log form").eq(logind).hasClass("sametime")){
 
 	}else{
@@ -180,46 +200,96 @@ function makechart(logind){
 		}else if($("#gender").val() == "f"){
 			cdata1 = fsf[Number($("#age").val())].split(",");
 		}
-		
-		$.each(cdata1,function(ind,val){
-			sum = 0;
-			
-			for(var i=0;i < $("#log form").length;i++){
-				if($("#log form").eq(logind + i).hasClass("sametime") || i == 0){
-					var thislog = $("#log form").eq(logind + i);
-					$.each($(thislog).children(".l-ing").children("div"),function(ind2,val2){
-						if(Number($(thislog).children(".l-ing").children("div").eq(ind2).data("fg")) == ind){
-							sum = sum + Number($(thislog).children(".l-ing").children("div").eq(ind2).children("p").eq(1).text());
+
+
+		if(op == 1){
+
+			sum = [0,0,0,0,0,0,0,0,0,0];
+			var bdc = "rgba(80,80,80,1)";
+			var bgc = "rgba(80,80,80,0.3)";
+			var allind = $("#log form,#log .daylog").index($("#log .daylog").eq(logind));
+			var timelist = $("#log .daylog").eq(logind).data("date").split(",")
+			for(var i=0;i < $("#log form,#log .daylog").length - 1 - allind;i++){
+				var thislog = $("#log form,#log .daylog").eq(allind + 1 + i)
+				if($("#log .daylog").eq(logind).data("date") == $(thislog).data("date") || i == 0){
+					if($(thislog).hasClass("sametime")){
+					}else{
+						var nutlist = $(thislog).data("nutritions");
+						for(var j=0;j < cdata1.length;j++){
+							var datasum = Number(sum[j]) + Number(nutlist[j]);
+							sum.splice(j,1,datasum);
 						}
-					});
+					}
+					
 				}else{
 					break;
 				}
 			}
-			
-			if(sum > cdata1[ind] / 3){
-				cdata2.push(100);
-			}else{
-				cdata2.push(sum * 300 / cdata1[ind]);
-			}
-			
-		});
+			$.each(sum,function(ind,val){
+				if(val > cdata1[ind]){
+					cdata2.push(100);
+				}else{
+					cdata2.push(val * 100/ cdata1[ind]);
+				}
+			});
+			var ctx = $("#log .daylog").eq(logind).children("canvas");
+		}else{
+			var nutlist = [];
+			$.each(cdata1,function(ind,val){
+				sum = 0;
+				
+				for(var i=0;i < $("#log form").length;i++){
+					if($("#log form").eq(logind + i).hasClass("sametime") || i == 0){
+						var thislog = $("#log form").eq(logind + i);
+						$.each($(thislog).children(".l-ing").children("div"),function(ind2,val2){
+							if(Number($(thislog).children(".l-ing").children("div").eq(ind2).data("fg")) == ind){
+								sum = sum + Number($(thislog).children(".l-ing").children("div").eq(ind2).children("p").eq(1).text());
+							}
+						});
+					}else{
+						break;
+					}
+				}
+				nutlist.push(sum);
+				if(sum > cdata1[ind] / 3){
+					cdata2.push(100);
+				}else{
+					cdata2.push(sum * 300 / cdata1[ind]);
+				}
+				
+			});
+			$("#log form").eq(logind).data("nutritions",nutlist);
+			$("#log form").eq(logind).attr("data-nutritions",nutlist);
 
-		var ctx = $("#log form").eq(logind).children(".l-chart").children(".b-sheet");
+			var ctx = $("#log form").eq(logind).children(".l-chart").children(".b-sheet");
+			if($("#log form").eq(logind).hasClass("breakfast")){
+				var bdc = "rgba(60,138,175,1)";
+				var bgc = "rgba(60,138,175,0.3)";
+			}else if($("#log form").eq(logind).hasClass("lunch")){
+				var bdc = "rgba(211,137,15,1)";
+				var bgc = "rgba(211,137,15,0.3)";
+			}else if($("#log form").eq(logind).hasClass("dinner")){
+				var bdc = "rgba(153,115,102,1)";
+				var bgc = "rgba(153,115,102,0.3)";
+			}else{
+				var bdc = "rgba(134,155,50,1)";
+				var bgc = "rgba(134,155,50,0.3)";
+			}
+		}
 		
 		new Chart(ctx, {
 			type:"radar",
 			data:{
-				labels:["乳・乳製品","卵","魚介・肉","豆・豆製品","野菜","いも類","果物","穀物","油脂","砂糖"],
+				labels:["乳・乳製品","卵","魚介・肉","豆・豆製品","野菜","いも類","果物","穀物"/*,"油脂","砂糖"*/],
 				datasets:[
 
 					{
-						label:"あなたの摂取量",
+						label:"あなたの摂取量(%)",
 						data:cdata2,
-						backgroundColor:"rgba(60,138,175,0.3)",
+						backgroundColor:bgc,
 						borderWidth:3,
-						pointBackgroundColor:"rgba(60,138,175,1)",
-						borderColor:"rgba(60,138,175,1)",
+						pointBackgroundColor:bdc,
+						borderColor:bdc,
 						pointBorderWidth:0
 					},
 					/*{
@@ -244,6 +314,7 @@ function makechart(logind){
 					},
 					ticks:{
 						min:0,
+						max:100,
 						stepSize:10
 					}
 				}
